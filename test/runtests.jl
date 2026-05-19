b6c22147fcf8caad32b97f76325bd622062050bc
Erik Schnetter <schnetter@gmail.com>
2026-05-19 15:41:00 -0400

    Horizon finder is much more robust

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using ApparentHorizonFinder
 using ForwardDiff
+using SpacetimeMetrics
 using StaticArrays
 using Test
 
@@ -26,6 +27,20 @@ function brill_lindquist_metric(p::SVector{3})
     return admvars
 end
 
+function kerr_schild_metric(p::SVector{3})
+    x, y, z = p
+    M = 1.0
+    a = 0.6
+    ks = KerrSchild(M, a)
+    t = 0
+    g, ∂g = dmetric(ks, SVector{4}(t, x, y, z))
+    K = ExtrinsicCurvature(ks, SVector{4}(t, x, y, z))
+    γ = SMatrix{3,3}(g[i,j] for i in 2:4, j in 2:4)
+    ∂γ = SArray{Tuple{3,3,3}}(∂g[i,j,k] for i in 2:4, j in 2:4, k in 2:4)
+    admvars = ADMVars(γ, ∂γ, K)
+    return admvars
+end
+
 @testset "Brill-Lindquist" begin
     x₀ = SVector{3}(0.0, 0.0, 0.1)
     N = 8
@@ -33,4 +48,13 @@ end
     atol = 1.0e-8
     maxiters = 100
     find_horizon(brill_lindquist_metric, x₀, N, r, atol, maxiters)
+end
+
+@testset "Kerr-Schild" begin
+    x₀ = SVector{3}(0.0, 0.0, 0.1)
+    N = 8
+    r = 2.0
+    atol = 1.0e-8
+    maxiters = 200
+    find_horizon(kerr_schild_metric, x₀, N, r, atol, maxiters)
 end
